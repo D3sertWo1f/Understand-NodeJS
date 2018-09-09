36044af505471dee686e78df3183e5ffb9fa8356
Adolf RJ <adolfojimenez1601@gmail.com>
2018-09-08 22:38:07 -0500

    init

---
+++ D2_FirstClassFunctions/Starter/app.js
@@ -0,0 +1,28 @@
+// function statement
+
+function greet () {
+    console.log("hi");
+}
+greet();
+
+//functions are first class
+
+function logGreeting(fn){
+      fn();
+}
+logGreeting(greet);
+
+//function expression
+
+var greetMe = function(){
+    console.log("Hello, Adolfo!");
+}
+greetMe();
+
+//it's first class
+logGreeting(greetMe);
+
+//use a function expression on the fly
+logGreeting(function(){
+    console.log("hello Adolf");
+});
