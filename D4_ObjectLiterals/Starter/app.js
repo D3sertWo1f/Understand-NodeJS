01f7de9e332422b5e2065a3c3fe6e6e0d2653d2d
Adolf RJ <adolfo.roman@outlook.com>
2018-09-08 23:43:28 -0500

    Object created

--- D4_ObjectLiterals/Starter/app.js
+++ D4_ObjectLiterals/Starter/app.js
@@ -1,1 +1,13 @@
 // Your Javascript Code Goes Here
+
+var person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    greet: function(){
+        console.log('Hello, ' + this.firstName + ' ' + this.lastName + '!');
+    }
+};
+
+person.greet()
+
+console.log(person['firstName']);
